5b64156f8fb20177a672644c6586b8bfadb82770
ZukaAbashidze <zukaabashidze70@gmail.com>
2024-06-04 22:18:15 +0400

    "first commit"

---
+++ GOA Projects/Level 01/homework/lession1.py
@@ -0,0 +1,12 @@
+name = "luka"
+surname = "lortkifanidze"
+age = 14
+height = 1.71 
+my = "chemi" 
+sa = "saxeli"
+ir = 'aris'
+im = " me var"
+gb = "da"
+
+
+print(my+" "+sa+" "+ir+" "+name +" "+ surname +" "+ im +" "+ str(age)  +" "+ gb  +" "+  im  +" "+ str(height))
