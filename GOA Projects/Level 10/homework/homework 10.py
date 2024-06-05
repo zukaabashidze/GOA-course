7e87dcc81ecce60224a556da36fb6e73cafbb119
ZukaAbashidze <zukaabashidze70@gmail.com>
2024-06-05 19:54:56 +0400

    "first commit"

---
+++ GOA Projects/Level 10/homework/homework 10.py
@@ -0,0 +1,35 @@
+# #0-10
+# for i in range(11):
+#   print(i)
+
+
+
+
+# #0-20
+# for i in range(21):
+#   print(i)
+
+
+
+# #0-100
+# for i in  range(101):
+#   print(i)
+
+
+
+
+
+
+
+#now this is game to find out your number 1-20(like Slots)
+num = int(input("please enter number(1-30)"))
+
+if num == 31 or num == 32 or num== 33 or num== 34 or num == 35 or num == 36 or num == 37 or  num == 38 or num == 39 or num== 40 :
+   print("Something went wrong please enter num 1-30")
+
+if num == 5 or num== 17 or num== 10 or num== 11 or num== 20 or num== 21  or  num== 22  or num==30: 
+      print("you won ")
+
+else:
+     print("you lost ")
+ 
