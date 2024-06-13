53a333761c9c374faaf5ba6a6f7e51b40290b3ae
ZukaAbashidze <zukaabashidze70@gmail.com>
2024-06-13 16:00:54 +0400

    "second commit"

---
+++ GOA Projects/Level 14/classwork/classwork 14.py
@@ -0,0 +1,22 @@
+# num = 0
+# while num != 10:
+#     print ("ZUKA")
+# num = num + 1
+
+
+# num = 0 
+# while num !=10:
+#     print(num)
+#     num = num + 2
+
+
+num = 0 
+while num != 5: 
+    num = int(input("please enter your number:" )) 
+    
+
+    if num == 5: 
+     print (" you won  !")
+
+    else:
+     print ("you lost")   
